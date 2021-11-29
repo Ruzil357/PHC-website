ca5e10a2fc35ccaef97adc9f3c1d7988458ab865
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2021-11-29 13:25:39 +0530

    Trying vercel

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # PSN Hack Club Website
 
-[![Netlify Status](https://api.netlify.com/api/v1/badges/18b91255-f446-4f21-909a-15f78fe0ca8e/deploy-status)](https://app.netlify.com/sites/psnhackclub/deploys)
+This the PSN Hack Club's official website.  
+  
+Made using:
+- Next.js
+- React.js
+- Scss
+- Vercel
 
-This the PSN Hack Club's official website.
-
-Visit the website [here](https://psnhackclub.netlify.app/)
+Visit the website [here](https://psnhackclub.vercel.app/)
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,12 +1,32 @@
-import {useEffect} from "react"
-import {useRouter} from "next/router"
+import React, {useEffect} from 'react'
+import styles from '../styles/NotFound.module.css'
+import Link from 'next/link'
+import MetaDecorator from '../components/MetaDecorator'
 
-export default function Custom404() {
-  const router = useRouter()
+function NotFound(props) {
+  useEffect(function onFirstMount() {
+    console.log(window.location)
+  }, []); // empty dependencies array means "run this once on first mount"
 
-  useEffect(() => {
-    router.replace("/notfound")
-  })
-
-  return null
+  return (
+    <div className={styles.container}>
+      <MetaDecorator
+        title={'Not Found'}
+        description={'The page you requested was not found.'}
+      />
+      <img
+        alt={'A very sad panther :('}
+        src={'/images/common/sad_panther.png'}
+        className={styles.panther}
+      />
+      <h1 className={'header text-center'}>404</h1>
+      <p className={'text-content text-center'}>
+        The page you requested does not exist
+        <br/>
+        <Link href={'/'}>Home</Link>
+      </p>
+    </div>
+  )
 }
+
+export default NotFound
