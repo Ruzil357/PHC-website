78f6e417d419b8add4af7ec745d5e03c2919f3d7
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2021-12-09 20:12:37 +0530

    change sytling on info text

--- src/data/Home.js
+++ src/data/Home.js
@@ -3,6 +3,5 @@ export const lines = [
   'Dunking on the Pathways Panther since 2020',
   'Is it too late to revert back to old commits?',
   'The only PSN Club still running!',
-  "All the tech kids at PSN cause we didn't have any other friends",
   "PSN's guild of spaghetti code writers",
 ]
