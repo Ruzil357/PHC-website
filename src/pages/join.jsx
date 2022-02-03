9f01c88d005149da1affee3ac67b109bc821689c
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2022-02-03 22:20:19 +0530

    added loader to compensate delay

--- src/pages/join.jsx
+++ src/pages/join.jsx
@@ -47,7 +47,8 @@ function Join() {
       setStatus({
         error: true,
         success: false,
-        message: err.response.data.msg || "There was an error processing the request!",
+        message:
+          err.response.data.msg || 'There was an error processing the request!',
         isUserError: true,
       })
     }
@@ -100,12 +101,34 @@ function Join() {
 
           <button
             disabled={inProgress || status.success}
-            className={`justify-center mt-2 block px-6 py-3 text-sm md:text-lg transition-colors duration-300 rounded-lg shadow-md text-white w-full ${
+            className={`inline-flex items-center justify-center mt-2 block px-6 py-3 text-sm md:text-lg transition-colors duration-300 rounded-lg shadow-md text-white w-full ${
               inProgress || status.success
                 ? 'bg-gray-400 shadow-gray-400/30 hover:cursor-not-allowed'
                 : 'bg-orange-500 hover:bg-red-500 shadow-red-400/30'
             }`}
           >
+            {inProgress && (
+              <svg
+                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                />
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                />
+              </svg>
+            )}
             Send Email
           </button>
         </form>
