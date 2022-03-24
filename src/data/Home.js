c7163617381fb757aee2076fdd931eb694d3da86
Ruzil357 <rushilgupta4@gmail.com>
2022-03-24 16:40:50 +0530

    DATA

--- src/data/Home.js
+++ src/data/Home.js
@@ -1,5 +1,3 @@
 export const lines = [
-  'Dunking on the Pathways Panther since 2020',
-  'Is it too late to revert back to old commits?',
-  'The only PSN Club still running!',
+  'Tagline ;)',
 ]
